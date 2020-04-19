8a17f6b6b0c6a0a873da18a363cfd6bdd74ca78e
mkamilhasan <mkamilhasan@users.noreply.github.com>
2020-04-19 15:10:51 +0700

    basic update
    update parent & child

--- list_child.h
+++ list_child.h
@@ -1,6 +1,39 @@
 #ifndef LIST_CHILD_H_INCLUDED
 #define LIST_CHILD_H_INCLUDED
+#define first_child(L) L.first_child
+#define last_child(L) L.last_child
+#define next_child(P) P->next_child
+#define prev_child(P) P->prev_child
+#define info_child(P) P->info_child
+#include <iostream>
 
+using namespace std;
+typedef string infotype_child;
+typedef struct elmlist_child *address_child;
+
+struct elmlist_child{
+    infotype_child info;
+    address_child next, prev;
+};
+
+struct List_child{
+    address_child first, last;
+};
+
+void createList(List_child &L);
+address_child allocate(infotype_child x);
+void deallocate(address_child &P);
+
+void insertFirst(List_child &L, address_child P);
+void insertAfter(List_child &L, address_child P, address_child &Prec);
+void insertLast(List_child &L, address_child P);
+
+void deleteFirst(List_child &L, address_child &P);
+void deleteAfter(List_child &L, address_child &P, address_child Prec);
+void deleteLast(List_child &L, address_child &P);
+
+address_child findElm(List_child L, infotype_child x);
+void printInfo(List_child L);
 
 
 #endif // LIST_CHILD_H_INCLUDED
--- list_parent.h
+++ list_parent.h
@@ -1,6 +1,36 @@
 #ifndef LIST_PARENT_H_INCLUDED
 #define LIST_PARENT_H_INCLUDED
+#define first_parent(L) L.first_parent
+#define next_parent(P) P->next_parent
+#define info_parent(P) P->info_parent
+#include <iostream>
 
+using namespace std;
+typedef int infotype_parent;
+typedef struct elmlist_parent *address_parent;
 
+struct elmlist_parent {
+    infotype_parent info;
+    address_parent next;
+};
+
+struct List_parent {
+    address_parent first;
+};
+
+void createList(List_parent &L);
+address_parent allocate(infotype_parent x);
+void deallocate(address_parent &P);
+
+void insertFirst(List_parent &L, address_parent P);
+void insertAfter(List_parent &L, address_parent P, address_parent Prec);
+void insertLast(List_parent &L, address_parent P);
+
+void deleteFirst(List_parent &L, address_parent &P);
+void deleteAfter(List_parent &L, address_parent &P, address_parent Prec);
+void deleteLast(List_parent &L, address_parent &P);
+
+void printInfo(List_parent L);
+address_parent findElm(List_parent L, infotype_parent x);
 
 #endif // LIST_PARENT_H_INCLUDED
