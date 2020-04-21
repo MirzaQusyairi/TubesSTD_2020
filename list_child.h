28f960918c2708db314cfe7d5222d9322eb783e9
MirzaQusyairi <mirza4909@gmail.com>
2020-04-21 08:19:28 +0700

    Header

--- list_child.h
+++ list_child.h
@@ -1,6 +1,38 @@
 #ifndef LIST_CHILD_H_INCLUDED
 #define LIST_CHILD_H_INCLUDED
+#include <iostream>
 
+using namespace std;
+
+struct infotypeChild {
+    string ID;
+    string namapasien;
+};
+
+typedef struct Child *address2;
+
+struct Child {
+    infotypeChild info;
+    address2 next;
+    address2 prev;
+};
+
+struct List2 {
+    address2 first;
+};
+
+void createListChild(List2 &L);
+address2 CreateElmChild(infotypeChild st);
+void insertFirstChild(List2 &L, address2 P);
+void insertAfterChild(List2 &L, address2 Prec, address2 P);
+void insertLastChild(List2 &L, address2 P);
+void insertSortChild(List2 &L, infotypeChild st);
+void deleteFirstChild(List2 &L, address2 &P);
+void deleteAfterChild(List2 &L, address2 Prec, address2 &P);
+void deleteLastChild(List2 &L, address2 &P);
+void deleteListChild(List2 &L, string st);
+address2 findElmChild(List2 &L, string X);
+void printInfoChild(List2 L);
 
 
 #endif // LIST_CHILD_H_INCLUDED
--- list_parent.h
+++ list_parent.h
@@ -1,6 +1,38 @@
 #ifndef LIST_PARENT_H_INCLUDED
 #define LIST_PARENT_H_INCLUDED
+#include <iostream>
 
+using namespace std;
+
+struct infotypeParent {
+    string ID;
+    string namadokter;
+};
+
+typedef struct Parent *address1;
+
+struct Parent {
+    infotypeParent info;
+    address1 next;
+};
+
+struct List1 {
+    address1 first;
+    address1 last;
+};
+
+void createListParent(List1 &L);
+address1 CreateElmParent(infotypeParent st);
+void insertFirstParent(List1 &L, address1 P);
+void insertAfterParent(List1 &L, address1 Prec, address1 P);
+void insertLastParent(List1 &L, address1 P);
+void insertSortParent(List1 &L, infotypeParent st);
+void deleteFirstParent(List1 &L, address1 &P);
+void deleteAfterParent(List1 &L, address1 Prec, address1 &P);
+void deleteLastParent(List1 &L, address1 &P);
+void deleteListParent(List1 &L, string st);
+address1 findElmParent(List1 &L, string X);
+void printInfoParent(List1 L);
 
 
 #endif // LIST_PARENT_H_INCLUDED
