7e2c732fa4e0c844a9a270af8702b4c2c0bf3b8f
mkamilhasan <mkamilhasan@users.noreply.github.com>
2020-04-26 10:12:40 +0700

    kamil update
    added basic main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
+#include "list_parent.h"
+#include "list_child.h"
+#include "list_relasi.h"
 #include <iostream>
-
 using namespace std;
+
+void mainmenu();
+void mainmenu(){
+    int pilih;
+    do{
+        cout<<"1. " <<endl;
+        cout<<"2. " <<endl;
+        cout<<"3. " <<endl;
+        cout<<"4. " <<endl;
+        cout<<"5. " <<endl;
+        cout<<"pilih menu: ";
+        cin>>pilih;
+        switch (pilih){
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        }
+    } while(true);
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    mainmenu();
     return 0;
 }
